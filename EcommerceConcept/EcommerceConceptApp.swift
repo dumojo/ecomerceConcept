d6e4835c045ca848d19c572a7e567370f902052d
dumojo <dumojo@icloud.com>
2022-12-02 16:20:24 +0400

    Initial Commit

---
+++ EcommerceConcept/EcommerceConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceConceptApp.swift
+//  EcommerceConcept
+//
+//  Created by Артем Дорожкин on 02.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
